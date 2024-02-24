15732071f6a9bc77d8c7a0552a7ec3c45cdcbcb6
Derfing <derendaevvkosta45@gmail.com>
2024-02-24 19:35:09 +0300

    feat(core): Install laravel/breeze lib and create home page

--- resources/views/templates/baseTemplate.blade.php
+++ resources/views/templates/baseTemplate.blade.php
@@ -19,7 +19,72 @@
 </head>
 
 <body>
+    <nav class="navigation-bar p-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-3">
+                    <a class="nav-link text-white" href="{{ route('home') }}">
+                        <h1>TaskManager</h1>
+                    </a>
+                </div>
+                <div class="col-3 my-auto">
+                    <a class="nav-link text-white" href="#">
+                        <h4>Главная</h4>
+                    </a>
+                </div>
+                <div class="col-2 my-auto">
+                    <a class="nav-link text-white" href="#">
+                        <h4>Задачи</h4>
+                    </a>
+                </div>
+                <div class="col-2 my-auto">
+                    <a class="nav-link text-white" href="#">
+                        <h4>Столы</h4>
+                    </a>
+                </div>
+                <div class="col-2 my-auto">
+                    @auth
+                        <a class="nav-link text-white" href="{{ route('profile') }}">
+                            <h4>Профиль</h4>
+                        </a>
+                    @endauth
+                    @guest
+                        <a class="nav-link text-white" href="{{ route('login') }}">
+                            <h4>Войти</h4>
+                        </a>
+                    @endguest
+                </div>
+            </div>
+        </div>
+    </nav>
     @yield('body')
+    <footer class="footer text-white py-4">
+        <hr>
+        <div class="container">
+            <div class="row">
+                <div class="col-6 text-left">
+                    <p class="mb-0">Добро пожаловать в Простой Менеджер Задач!</p>
+                    <p class="mb-0">Этот проект разработан мной, известным как Derfing, как мой личный пет-проект для
+                        управления задачами и проектами.</p>
+                </div>
+                <div class="col-6 text-right my-auto">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item mr-3">
+                            <a href="https://github.com/Derfing/LaravelSimpleTaskManager" target="_blank"
+                                class="text-white">GitHub проекта</a>
+                        </li>
+                        <li class="list-inline-item mr-3">
+                            <a href="#" class="text-white">Политика конфиденциальности</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="text-white">Условия использования</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TableController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,22 +18,32 @@ use App\Http\Controllers\TableController;
 |
 */
 
+Route::view('/', 'home')->middleware('guest')->name('home');
+
 Route::group(['prefix' => 'test'], function () {
     Route::get('get', [TestController::class, 'get']);
     Route::post('post', [TestController::class, 'post']);
 });
 
-Route::group(['prefix' => 'user'], function () {
+Route::group(['prefix' => 'user', 'middleware' => 'auth'], function () {
     Route::get('/create', [UserController::class, 'createUser']);
     Route::get('/{id}', [UserController::class, 'getUser']);
 });
 
-Route::group(['prefix' => 'task'], function () {
+Route::group(['prefix' => 'task', 'middleware' => 'auth'], function () {
     Route::get('/create', [TaskController::class, 'createTask']);
     Route::get('/{id}', [TaskController::class, 'getTask']);
 });
 
-Route::group(['prefix' => 'table'], function () {
+Route::group(['prefix' => 'table', 'middleware' => 'auth'], function () {
     Route::get('/create', [TableController::class, 'createTable']);
     Route::get('/{id}', [TableController::class, 'getTable']);
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
